512a73b72aa37abc37c853abec9eb871719553c1
TamaraRibeiro <tamara.fl@hotmail.com>
2023-06-30 12:24:09 -0400

    First commit

---
+++ src/components/Content/content.styled.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { styled } from "styled-components";
+
+export const Section = styled.section`
+  margin: 4rem 1rem;
+  justify-content: center;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const DivOne = styled.div`
+  margin: 2%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
